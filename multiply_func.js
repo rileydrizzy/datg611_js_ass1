67107f7b2aa867b3cfc383eee04a7a2947fc7e14
Ladipo Ezekiel Ipadeola <105759894+rileydrizzy@users.noreply.github.com>
2026-02-20 23:29:30 +0000

    docs: add documentation for file and its functions

--- multiply_func.js
+++ multiply_func.js
@@ -1,14 +1,23 @@
+/**
+ * @file multiply_func.js
+ * @description This file contains a utility function to multiply a variable number of numeric arguments.
+ * It demonstrates the use of the rest parameter syntax and array reduction in JavaScript.
+ */
 
-
+/**
+ * Multiplies all provided numeric arguments.
+ * 
+ * @param {...number} numbers - A variable number of arguments to be multiplied.
+ * @returns {number} The product of all arguments. Returns 0 if no arguments are provided.
+ */
 function multiplyArguments(...numbers) {
   // Return 0 if no arguments are passed
   if (numbers.length === 0) return 0;
-  
-  // Multiply all numbers in the array
+
   return numbers.reduce((product, currentNumber) => product * currentNumber, 1);
 }
 
 // Examples:
-console.log(multiplyArguments(2, 3));     
-console.log(multiplyArguments(2, 3, 4, 5)); 
+console.log(multiplyArguments(2, 3));
+console.log(multiplyArguments(2, 3, 4, 5));
 console.log(multiplyArguments(7));         
